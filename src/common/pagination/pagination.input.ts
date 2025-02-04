f34c62f129991b6ee8d99823b1545a1716de4cfa
bijaya143 <nemomajhi14@gmail.com>
2025-02-04 16:42:33 +0545

    Default page is 1

--- src/common/pagination/pagination.input.ts
+++ src/common/pagination/pagination.input.ts
@@ -3,7 +3,7 @@ import { IsNumber, IsOptional } from 'class-validator';
 export class PaginationInput {
   @IsOptional()
   @IsNumber()
-  page?: number = 1;
+  page?: number = 1; // Default page is 1
   @IsOptional()
   @IsNumber()
   limit?: number = 10;
